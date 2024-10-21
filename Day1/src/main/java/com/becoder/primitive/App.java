141023477d892eefbef5cf520213fc756f249796
Avanish-Yadav <avanishyadav1906@gmail.com>
2024-10-21 15:48:30 +0530

    Dependency injection setter-primitive

---
+++ Day1/src/main/java/com/becoder/primitive/App.java
@@ -0,0 +1,13 @@
+package com.becoder.primitive;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String args[]) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("com/becoder/primitive/configure.xml");
+		student st1=context.getBean("st1",student.class);
+		student st2=context.getBean("st2",student.class);
+		System.out.println(st1);
+		System.out.println(st2);
+	}
+}
