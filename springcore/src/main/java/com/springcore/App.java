abe04c9f1e022ac5155bd7b3922b8bd8ebdf66cc
ajeet <ajeet@Avanish>
2024-11-26 11:09:20 +0530

    Second commit

--- springcore/src/main/java/com/springcore/App.java
+++ springcore/src/main/java/com/springcore/App.java
@@ -9,11 +9,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
         System.out.println( "Hello World!" );
         ApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
         Student student1=(Student)context.getBean("student1");
         System.out.println(student1);
+        
+        Student student2=(Student)context.getBean("student2");
+        System.out.println(student2);
+        
+        Student student3=(Student)context.getBean("student3");
+        System.out.println(student3);
     }
 }
